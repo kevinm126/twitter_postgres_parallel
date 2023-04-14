490bfc500903b4e01c862ddeb87cf6f9cf689c02
kevinm126 <kmarin25@cmc.edu>
2023-04-13 23:21:39 -0700

    first commit lets see if it actually works

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -6,13 +6,13 @@ echo '==========================================================================
 echo 'load pg_denormalized'
 echo '================================================================================'
 # FIXME: implement this
-
+time echo "$files" | parallel bash load_denormalized.sh
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-echo "$files" | time parallel python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:2/ --inputs
+echo "$files" | time parallel python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:2212/ --inputs
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-echo "$files" | time parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:3/ --inputs
+echo "$files" | time parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:3312/ --inputs
